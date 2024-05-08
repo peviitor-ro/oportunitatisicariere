b0f10dafbd47c99ec69a95246ac7676f958d846d
MisuV13 <vatulescu.mihai@gmail.com>
2024-05-08 19:23:16 +0300

    Added laptop responsive breakpoint for the carousel in the script file

--- carousel.js
+++ carousel.js
@@ -1,13 +1,49 @@
-// Initialize Swiper and define the carousel structure:
-new Swiper(".how-contribute__jobs", {
-  slidesPerView: 4,
-  spaceBetween: 40,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
+const carouselState = {
+  numOfSlides: 4,
+  slidesSpace: 40,
+};
+
+const endpointSizes = {
+  laptop: "(min-width: 1024px) and (max-width: 1398px)",
+};
+
+//Avoid object properties values to be changed:
+Object.freeze(endpointSizes);
+
+function initializeSwiper(carouselConfig) {
+  // Initialize Swiper and define the carousel structure:
+  new Swiper(".how-contribute__jobs", {
+    slidesPerView: carouselConfig.numOfSlides,
+    spaceBetween: carouselConfig.slidesSpace,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  });
+}
+
+//Initialize swiper function for its first rendering on the page:
+initializeSwiper(carouselState);
+
+const laptopEndpoint = window.matchMedia(endpointSizes.laptop);
+
+function handleCarouselResponsiveness(responsiveEndpoint) {
+  if (responsiveEndpoint.matches) {
+    initializeSwiper({
+      numOfSlides: 3,
+      slidesSpace: 30,
+    });
+  } else {
+    initializeSwiper(carouselState);
+  }
+}
+
+handleCarouselResponsiveness(laptopEndpoint);
+
+document.addEventListener("change", () => {
+  handleCarouselResponsiveness(laptopEndpoint);
 });
