ca12e645aa9271d55855d3a2955c34edb2be8e9c
AdelinaMoroaca <moroacaadelina@gmail.com>
2024-06-15 16:59:01 +0300

    Add new style for navbar

--- script.js
+++ script.js
@@ -92,3 +92,65 @@ function hideMenuContent() {
 
 }
 document.querySelector('.close-menu-icon').addEventListener('click', hideMenuContent);
+
+
+
+function createNewSubList() {
+  const newList = document.createElement('ul');
+
+  const newItems = [
+      { href: "https://www.linkedin.com/company/asociatia-oportunitati-si-cariere/", src: "./assets/social-media-icons/linkedin_icon.svg", text: "linkedin" },
+      { href: "https://www.instagram.com/peviitor.ro/", src: './assets/social-media-icons/instagram_icon.svg',text: "instagram" },
+      { href: "https://discord.gg/KPMkdUfQNu", src: './assets/social-media-icons/discord.svg', text: "discord" },
+      { href: "https://github.com/peviitor-ro/oportunitatisicariere/issues", src: './assets/social-media-icons/github_icon.svg', text: "github" },
+      { href: "https://meet.jit.si/PEVIITOR.RO", src: "./assets/jitsi.svg", text: "jit" },
+      { href: "https://dev.to/t/peviitor", src: "./assets/social-media-icons/dev_icon.svg", text: "dev" }
+  ];
+
+  //style that will be added in SASS
+  newList.style.display = 'flex';
+  newList.style.gap = '20px';
+  newList.style.listStyleType = 'none';
+
+  newItems.forEach(item => {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      const img = document.createElement('img');
+
+      a.href = item.href;
+      a.target = 'blank';
+
+      img.src = item.src;
+      img.alt = item.text;
+      img.style.width = '24px'; 
+      img.style.height = '24px';
+
+      a.appendChild(img);
+      li.appendChild(a);
+      newList.appendChild(li);
+  });
+
+  return newList;
+}
+
+
+function insertNewListBeforeButton() {
+  const navList = document.querySelector('.nav__links__list.nav__links__nav');
+  const items = navList.children;
+
+  let buttonListItem;
+  for (let item of items) {
+      if (item.querySelector('button')) {
+          buttonListItem = item;
+          break;
+      }
+  }
+
+  const newList = createNewSubList();
+
+  if (buttonListItem) {
+      navList.insertBefore(newList, buttonListItem);
+  }
+}
+
+insertNewListBeforeButton();
