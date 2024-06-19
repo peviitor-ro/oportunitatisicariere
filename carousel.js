8c7c8b310aefbfd2c55de1b5c5ea3e2076a6be4e
AndreiBarari <>
2024-06-19 05:23:50 +0000

    Deploying to gh-pages from @ peviitor-ro/oportunitatisicariere@c372c196f3ea4d4ad0b7b683f7f4a5bef4418c23 🚀

--- carousel.js
+++ carousel.js
@@ -5,18 +5,21 @@ const carouselState = {
 
 const deviceSizes = {
   laptop: "(min-width: 1024px) and (max-width: 1398px)",
-  smallLaptop: "(min-width: 769px) and (max-width: 1150px)",
+  smallLaptop: "(min-width: 769px) and (max-width: 1024px)",
   tablet: "(min-width: 640px) and (max-width: 768px)",
-  phone: "(min-width: 320px) and (max-width: 640px)",
-  smallPhone: "(max-width: 320px)",
+  phone: "(max-width: 639px)",
 };
 
-//Avoid object properties values to be changed:
+// Avoid object properties values to be changed:
 Object.freeze(deviceSizes);
 
+let swiper;
+
 function initializeSwiper(carouselConfig) {
-  // Initialize Swiper and define the carousel structure:
-  new Swiper(".how-contribute__jobs", {
+  if (swiper) {
+    swiper.destroy(true, true);
+  }
+  swiper = new Swiper(".how-contribute__jobs", {
     slidesPerView: carouselConfig.numOfSlides,
     spaceBetween: carouselConfig.slidesSpace,
     navigation: {
@@ -30,41 +33,45 @@ function initializeSwiper(carouselConfig) {
   });
 }
 
-//Initialize swiper function for its first rendering on the page:
+// Initialize swiper function for its first rendering on the page:
 initializeSwiper(carouselState);
 
 const pageEndpoints = {
-  //We don't need a desktop endpoint since the desktop is default for carousel state config:
+  // We don't need a desktop endpoint since the desktop is default for carousel state config:
   laptop: window.matchMedia(deviceSizes.laptop),
   smallLaptop: window.matchMedia(deviceSizes.smallLaptop),
   tablet: window.matchMedia(deviceSizes.tablet),
   phone: window.matchMedia(deviceSizes.phone),
 };
 
-function handleCarouselResponsiveness(responsiveEndpoint) {
-  if (responsiveEndpoint.laptop.matches) {
+function handleCarouselResponsiveness() {
+  if (pageEndpoints.laptop.matches) {
     initializeSwiper({
       numOfSlides: 3,
-      slidesSpace: 5,
+      slidesSpace: 20,
     });
-  }
-
-  if (
-    responsiveEndpoint.smallLaptop.matches ||
-    responsiveEndpoint.tablet.matches
-  ) {
+  } else if (pageEndpoints.smallLaptop.matches) {
+    initializeSwiper({
+      numOfSlides: 3,
+      slidesSpace: 20,
+    });
+  } else if (pageEndpoints.tablet.matches) {
     initializeSwiper({
       numOfSlides: 2,
-      slidesSpace: 0,
+      slidesSpace: 10,
     });
-  }
-
-  if (responsiveEndpoint.tablet.matches || responsiveEndpoint.phone.matches) {
+  } else if (pageEndpoints.phone.matches) {
     initializeSwiper({
       numOfSlides: 1,
-      slidesSpace: 0,
+      slidesSpace: 10,
     });
+  } else {
+    initializeSwiper(carouselState);
   }
 }
 
-handleCarouselResponsiveness(pageEndpoints);
+// Check when the window resizes and reinitialize swiper accordingly:
+window.addEventListener("resize", handleCarouselResponsiveness);
+
+// Call the handler to set the initial state:
+handleCarouselResponsiveness();
