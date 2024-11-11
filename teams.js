4addd674f5b4d5705e3d0c3bb7dbcffa4f5ab309
AdelinaMoroaca <>
2024-11-11 08:26:16 +0000

    Deploying to gh-pages from @ peviitor-ro/oportunitatisicariere@e6d0ccb509eba804ad2fa1b152a44b52546b1e69 🚀

--- teams.js
+++ teams.js
@@ -11,13 +11,17 @@ function loadTeamMembers() {
       teamMembers = data;
       initializeTeams(); // Initiazare echipe dupa ce datele devin disponibile
     })
-    .catch((error) => console.error("Eroare la încărcarea JSON:", error));
+    .catch((error) => console.error("Eroare la încarcarea JSON:", error));
 }
 
 // Functie pentru initializarea echipelor
 function initializeTeams() {
-  // Vector pentru bara de navigare echipe, in functie de un numar unic de echipe
-  const teams = [...new Set(teamMembers.map((teamMember) => teamMember.team))];
+  // Vector pentru bara de navigare echipe, cu echipe unice extrase din rolurile membrilor
+  const teams = [
+    ...new Set(
+      teamMembers.flatMap((member) => member.roles.map((role) => role.team))
+    ),
+  ];
 
   let firstButton = null; // Variabila pentru a retine primul buton creat
 
@@ -29,7 +33,7 @@ function initializeTeams() {
 
     if (!firstButton) firstButton = button;
     button.addEventListener("click", () => {
-      window.location.href = "#teamRooster"; // Redirectionare spre inceputul sectiunii de afisare a echipei
+      window.location.href = "#teamRooster"; // Redirectionare spre începutul sectiunii de afisare a echipei
     });
 
     // Event listener pentru selectarea echipei
@@ -40,87 +44,87 @@ function initializeTeams() {
         .forEach((btn) => btn.classList.remove("active"));
       button.classList.add("active");
 
-      // Afisarea membrilor echipei selectate in functie de butonul activat
+      // Afisarea membrilor echipei selectate în functie de butonul activat
       displayTeamMembers(team);
     });
 
-    // Adaugam butonul in bara de navigare echipe
+    // Adaugam butonul în bara de navigare echipe
     teamNavigation.appendChild(button);
   }
 
   // Setarea primului buton generat ca fiind activ, standard membrii "Principali"
   if (firstButton) {
     firstButton.classList.add("active");
-
-    // Apelarea functiei de afisare a membrilor echipei selectate, in functie de butonul activat
+    // Apelarea functiei de afisare a membrilor echipei selectate, în functie de butonul activat
     displayTeamMembers(firstButton.textContent);
   }
 }
 
 // Functia de afisare a membrilor echipei
-function displayTeamMembers(team) {
+function displayTeamMembers(selectedTeam) {
   membersWrapper.innerHTML = ""; // Golim continutul anterior pentru afisarea doar a membrilor corespunzatori
 
-  // Filtrarea membrilor
-  const filteredMembers =
-    team === "All"
-      ? teamMembers
-      : teamMembers.filter((member) => member.team === team);
+  // Filtrarea membrilor care apartin echipei selectate
+  const filteredMembers = teamMembers.filter((member) =>
+    member.roles.some((role) => role.team === selectedTeam)
+  );
 
-  // Sortare pentru a avea Team Leaders la inceput
-  const sortedMembers = filteredMembers.sort((a, b) => b.teamLead - a.teamLead);
+  // Sortare pentru a avea Team Leaders la început
+  const sortedMembers = filteredMembers.sort((a, b) => {
+    const aIsLeader = a.roles.some(
+      (role) => role.team === selectedTeam && role.teamLead
+    );
+    const bIsLeader = b.roles.some(
+      (role) => role.team === selectedTeam && role.teamLead
+    );
+    return bIsLeader - aIsLeader; // Sortare descrescatoare pe baza `teamLead`
+  });
 
   // Afisarea membrilor
   for (let member of sortedMembers) {
-    // Crearea card
-    const card = document.createElement("div");
-    card.classList.add("team__member");
+    const role = member.roles.find((role) => role.team === selectedTeam);
+    if (role) {
+      // Crearea cardului
+      const card = document.createElement("div");
+      card.classList.add("team__member");
 
-    // Adaugarea clasa team-leader
-    if (member.teamLead) {
-      card.classList.add("team-leader");
+      // Adaugarea clasei `team-leader`
+      if (role.teamLead) {
+        card.classList.add("team-leader");
+      }
+
+      // Crearea continutului cardului
+      card.innerHTML = `
+             <img
+                    src="${member.avatar}"
+                    alt=""
+                    class="team__member-img"
+                  />
+                  <h4>${member.name}</h4>
+                  <p>${role.position}</p>
+                  <div class="team__member__socials">
+                  ${
+                    member.socials.linkedin
+                      ? `<span><a href="${member.socials.linkedin}" target="_blank"></a></span>`
+                      : ""
+                  }
+                  ${
+                    member.socials.github
+                      ? `<span><a href="${member.socials.github}" target="_blank"></a></span>`
+                      : ""
+                  }
+                  ${
+                    member.socials.discord
+                      ? `<span><a href="${member.socials.discord}" target="_blank"></a></span>`
+                      : ""
+                  }
+                  </div>
+                  `;
+      // Adaugarea cardului în wrapper-ul pentru membrii
+      membersWrapper.appendChild(card);
     }
-
-    // Creare continut card
-    card.innerHTML = `
-           <img
-                  src="${member.avatar}"
-                  alt=""
-                  class="team__member-img"
-                />
-                <h4>${member.name}</h4>
-                <p>${member.position}</p>
-                <div class="team__member__socials">
-                ${
-                  member.socials.linkedin
-                    ? `
-                  <span>
-                  <a
-                  href="${member.socials.linkedin}"
-                  target="_blank"
-                  ></a></span>
-                  `
-                    : ""
-                }
-                ${
-                  member.socials.github
-                    ? `<span><a href="${member.socials.github}" target="_blank">
-                  </a></span>`
-                    : ""
-                }
-                ${
-                  member.socials.discord
-                    ? `<span><a
-                    href="${member.socials.discord}"
-                    target="_blank"
-                  ></a></span>`
-                    : ""
-                }`;
-
-    // Adaugare card in wrapperul pentru membrii
-    membersWrapper.appendChild(card);
   }
 }
 
-// Incarcarea membrilor dupa incarcarea paginii
+// Încarcarea membrilor dupa încarcarea paginii
 document.addEventListener("DOMContentLoaded", loadTeamMembers);
