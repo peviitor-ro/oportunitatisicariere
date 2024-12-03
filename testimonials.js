ef96f8d6913bc63ef65e7a10a08eaf5c79fd8d80
Anca Matei <98110730+Anca2022@users.noreply.github.com>
2024-12-03 15:06:09 +0200

    added swiper for stories, for screens above 1024px

--- testimonials.js
+++ testimonials.js
@@ -8,23 +8,46 @@ document.addEventListener("DOMContentLoaded", () => {
         data.forEach((testimonial) => {
           const { name, avatar, role, text } = testimonial;
           storiesContainer.innerHTML += `
-        <div class="swiper-slide">      <div class="stories-individual ">
-            <div class="stories-left">
-              <img src="./assets/povesti-quote.svg" alt="" />
-              <p>
-               ${text}
-              </p>
-              <div class="stories-name">
-                <h3>${name}</h3>
-                <p>${role}</p>
+          <div class="swiper-slide">      
+            <div class="stories-individual">
+              <div class="stories-left">
+                <img src="./assets/povesti-quote.svg" alt="" />
+                <p>
+                ${text}
+                </p>
+                <div class="stories-name">
+                  <h3>${name}</h3>
+                  <p>${role}</p>
+                </div>
+                <p class="stories-extra">povești de succes</p>
               </div>
-              <p class="stories-extra">povești de succes</p>
+              <div class="stories-right">
+                <img src=${avatar} alt="" />
+              </div>
             </div>
-            <div class="stories-right">
-              <img src=${avatar} alt="" />
-            </div>
-          </div>
           </div>`;
+        });
+
+        const storySwiper = new Swiper(".stories-container", {
+          effect: "cards",
+          cardsEffect:{
+            rotate: 40,
+            depth: 400,
+            modifier: 3,
+            slideShadows: false,
+          },
+          grabCursor: true,
+          rewind: true,
+          mousewheel: true,
+          pagination: {
+            el: ".stories-pagination",
+            clickable: true,
+          },
+          speed: 1000,
+          direction: 'vertical',
+          autoplay: {
+            delay: 5000, //viteza cu care se schimba slideurile 
+          },
         });
       })
       .catch((error) => console.error("Eroare la încarcarea JSON:", error));
