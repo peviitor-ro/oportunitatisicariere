c95d7bd5bf13ba54b9a0959cc7f118628f9a349a
AndreiBarari <>
2025-02-15 16:11:27 +0000

    Deploying to gh-pages from @ peviitor-ro/oportunitatisicariere@8ec87709ce6ebeb8b7f16b19793a9d80a36e9825 🚀

--- jobCards.js
+++ jobCards.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   let jobsData = [];
 
-  fetch("data/data.json")
+  fetch('./data/data.json')
     .then((response) => response.json())
     .then((data) => {
       jobsData = data;
