8cc527f4c786b8057c914a6ab713123bbeb25e21
AndreiBarari <>
2025-12-18 09:11:53 +0000

    Deploying to gh-pages from @ peviitor-ro/oportunitatisicariere@cca7b7b74c5c9f421f00ea0eda67764bd4d6c9d6 🚀

---
+++ script_404.js
@@ -0,0 +1,46 @@
+const redirectBtn = document.getElementById("redirect-btn");
+
+const routes = {
+  "": "home",
+  "our-mission": "ourMission",
+  "how-contribute": "howContribute",
+  "why-contribute": "whyContribute",
+  teamList: "team-rooster",
+  team: "team",
+  schedule: "schedule",
+  stories: "stories",
+};
+
+function loadContent() {
+  const path = window.location.hash.substring(1);
+
+  if (
+    path &&
+    !routes[path] &&
+    !window.location.pathname.includes("index.html")
+  ) {
+    window.location.href = "404.html";
+    return;
+  }
+
+  if (path) {
+    setTimeout(() => {
+      const targetElement = document.querySelector(`#${path}`);
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: "smooth" });
+      }
+    }, 300);
+  }
+}
+
+window.addEventListener("load", () => {
+  loadContent();
+});
+
+window.addEventListener("hashchange", loadContent);
+
+function redirectToHome() {
+  window.location.href = "/index.html";
+}
+
+redirectBtn?.addEventListener("click", redirectToHome);
